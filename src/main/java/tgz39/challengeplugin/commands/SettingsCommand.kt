7fc35a0dc35106b269e9a7b0f9f1c4de7d8f4475
TgZ39 <badbaboimbus+github@gmail.com>
2023-10-05 19:16:41 +0200

    changed error messages from german to english

--- src/main/java/tgz39/challengeplugin/commands/SettingsCommand.kt
+++ src/main/java/tgz39/challengeplugin/commands/SettingsCommand.kt
@@ -4,19 +4,21 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import tgz39.challengeplugin.settingsGUI
+import tgz39.challengeplugin.Main
 
 class SettingsCommand : CommandExecutor{
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
 
+        val settingsGUI = Main.settingsGUI
+
         if (sender !is Player) {
-            sender.sendMessage("Du musst ein Spieler sein um diesen Command benutzen zu können.")
+            sender.sendMessage("You need to be a player to run this command.")
             return false
         }
         val player: Player = sender
 
         if (!player.hasPermission("challengeplugin.commands.settings")) {
-            player.sendMessage("Du darfst diesen Command nicht ausführen.")
+            player.sendMessage("You are not allowed to run this command.")
             return false
         }
 
--- src/main/java/tgz39/challengeplugin/commands/TimerCommand.kt
+++ src/main/java/tgz39/challengeplugin/commands/TimerCommand.kt
@@ -1,23 +1,60 @@
 package tgz39.challengeplugin.commands
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
-import org.bukkit.entity.Player
+import tgz39.challengeplugin.Main
+import java.lang.NumberFormatException
 
 class TimerCommand : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
 
-        if (sender !is Player) {
-            sender.sendMessage("Du musst ein Spieler sein um diesen Command benutzen zu können.")
+        val timer = Main.timer
+
+        if (!sender.hasPermission("challengeplugin.commands.timer")) {
+            sender.sendMessage("You are not allowed to run this command.")
             return false
         }
-        val player: Player = sender
 
-        if (!player.hasPermission("challengeplugin.commands.timer")) {
-            player.sendMessage("Du darfst diesen Command nicht ausführen.")
-            return false
+        if (args?.size == 0) {
+            sender.sendMessage(Component.text("Usage: /timer [resume, pause, reset, set]"))
+        }
+
+        when (args?.get(0)?.lowercase()) {
+            "resume" -> {
+                timer.isActive = true
+                sender.sendMessage(Component.text("Timer has been enabled."))
+            }
+
+            "pause" -> {
+                timer.isActive = false
+                sender.sendMessage(Component.text("Timer has been paused."))
+            }
+
+            "reset" -> {
+                timer.isActive = false
+                timer.time = 0
+                timer.ticks = 0
+                sender.sendMessage(Component.text("Timer has been reset."))
+            }
+
+            "set" -> {
+                if (args.size < 2) {
+                    sender.sendMessage(Component.text("Usage: /timer set <SECONDS>"))
+                    return false
+                }
+                try {
+                    timer.time = args[1].toInt()
+
+                } catch (e: NumberFormatException) {
+                    sender.sendMessage(Component.text("Usage: /timer set <SECONDS>"))
+                    return false
+                }
+            }
         }
 
         return false
@@ -25,12 +62,13 @@ class TimerCommand : CommandExecutor, TabCompleter {
 
     override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>?): MutableList<String> {
 
-        var list: MutableList<String> = mutableListOf()
+        val list: MutableList<String> = mutableListOf()
 
         if (args?.size == 1) {
             list.add("resume")
-            list.add("stop")
+            list.add("pause")
             list.add("reset")
+            list.add("set")
         }
 
         return list
