6e10cf3198a74ea02993b8fc721051e0d9ae6edc
TgZ39 <badbaboimbus+github@gmail.com>
2023-11-29 20:59:42 +0100

    implement onEnable() and onDisable() methods for Challenges

--- src/main/java/tgz39/challengeplugin/utils/Challenge.kt
+++ src/main/java/tgz39/challengeplugin/utils/Challenge.kt
@@ -1,10 +1,37 @@
 package tgz39.challengeplugin.utils
 
 import org.bukkit.inventory.ItemStack
+import tgz39.challengeplugin.timer.Timer
 
-interface Challenge {
+abstract class Challenge {
 
-    var isActive: Boolean
-    fun guiItem(): ItemStack
+    init {
+        ChallengeAction.addChallenge(this)
+    }
 
+    abstract var isActive: Boolean
+    abstract fun guiItem(): ItemStack
+
+    open fun onEnable() {}
+    open fun onDisable() {}
+}
+
+object ChallengeAction {
+    val challenges: MutableList<Challenge> = mutableListOf()
+
+    fun enable() {
+        challenges.forEach {
+            if (it.isActive && Timer.isActive) it.onEnable()
+        }
+    }
+
+    fun disable() {
+        challenges.forEach {
+            if (!it.isActive || !Timer.isActive) it.onDisable()
+        }
+    }
+
+    fun addChallenge(challenge: Challenge) {
+        challenges.add(challenge)
+    }
 }
